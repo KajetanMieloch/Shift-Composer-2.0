637a3332aac5a4060a4688b298cf605b8af29c07
KajetanMieloch <kajetanmieloch@gmail.com>
2023-10-09 18:26:42 +0200

    added new app dashboard.

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.contrib.auth import views as auth_views
 from .forms import LoginForm
 
+app_name = 'core'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
