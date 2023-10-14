ba2cfb6552e1545e0a0fb0434606c51ff73e6124
KajetanMieloch <kajetanmieloch@gmail.com>
2023-10-14 08:50:25 +0200

    Started process of adding organisations

--- organisation/urls.py
+++ organisation/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
     path('join/', views.join, name='join'),
+    path('leave/', views.leave, name='leave'),
+    path('delete/', views.delete, name='delete'),
 ]
--- organisation/views.py
+++ organisation/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from core.models import UserProfile
 from .models import Organisation
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 
 
 @login_required
@@ -10,11 +11,28 @@ def index(request):
     return render(request, 'organisation/index.html', {
         'user': request.user,
         'in_organisation': UserProfile.objects.get(user=request.user).in_organisation,
+        'is_admin': UserProfile.objects.get(user=request.user).is_admin,
         'organisation': Organisation.objects.get(members__user=request.user),
         })
 
 def create(request):
-    return render(request, 'organisation/create.html')
+    return render(request, 'organisation/create.html', {
+        'user': request.user,
+        'in_organisation': UserProfile.objects.get(user=request.user).in_organisation,
+        'is_admin': UserProfile.objects.get(user=request.user).is_admin,
+        'organisation': Organisation.objects.get(members__user=request.user),
+        })
 
 def join(request):
     return render(request, 'organisation/join.html')
+
+def leave(request):
+    user_profile = UserProfile.objects.get(user=request.user)
+    user_profile.in_organisation = False
+    user_profile.is_admin = False
+    user_profile.save()
+
+    return redirect('organisation:index')
+
+def delete(request):
+    return render(request, 'organisation/delete.html')
